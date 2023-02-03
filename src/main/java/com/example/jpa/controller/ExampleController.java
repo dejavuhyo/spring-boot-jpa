167c65291408c2f5ef8a386e8f8595f8d60de91a
공대베짱이 <dejavuhyo@gmail.com>
2023-02-03 16:47:25 +0900

    docs: update controller

--- src/main/java/com/example/jpa/controller/ExampleController.java
+++ src/main/java/com/example/jpa/controller/ExampleController.java
@@ -1,13 +1,22 @@
 package com.example.jpa.controller;
 
 import com.example.jpa.dto.ExampleDto;
-import com.example.jpa.model.Example;
+import com.example.jpa.entity.Example;
 import com.example.jpa.service.ExampleService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +28,6 @@ import java.util.stream.Collectors;
 public class ExampleController {
     @Autowired
     ExampleService exampleService;
-
     @Autowired
     ModelMapper modelMapper;
 
@@ -66,7 +74,7 @@ public class ExampleController {
      *
      * @param exampleDto
      */
-    @@PutMapping(value = "/{id}")
+    @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void put(@PathVariable("id") String id, @RequestBody ExampleDto exampleDto) {
         exampleDto.setId(id);
