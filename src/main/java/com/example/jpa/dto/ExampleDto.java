c2f3ba8950cdf5823e2a9c4da99c13cdb8f39fc2
공대베짱이 <dejavuhyo@gmail.com>
2023-02-03 16:47:54 +0900

    docs: update dto

--- src/main/java/com/example/jpa/dto/ExampleDto.java
+++ src/main/java/com/example/jpa/dto/ExampleDto.java
@@ -2,10 +2,8 @@ package com.example.jpa.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@ToString
 @NoArgsConstructor
 public class ExampleDto {
     private String id;
