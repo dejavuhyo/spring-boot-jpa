36baa9747df57fdb9f7795ada945fc0980f2eb7a
공대베짱이 <dejavuhyo@gmail.com>
2023-02-03 16:48:36 +0900

    docs: update entity

---
+++ src/main/java/com/example/jpa/entity/Example.java
@@ -0,0 +1,26 @@
+package com.example.jpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Example {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+}
